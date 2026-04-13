7daf4482221add5134ddd000474afafe6445c72d
vamsikoneru06 <koneru.mohanvamsi894@gmail.com>
2026-04-13 16:03:40 +0530

    [vamsikoneru06] added UC15

---
+++ App/src/main/CargoSafetyException.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Author: vamsikoneru06
+ */
+public class CargoSafetyException extends RuntimeException {
+    public CargoSafetyException(String msg){
+        super(msg);
+    }
+}
